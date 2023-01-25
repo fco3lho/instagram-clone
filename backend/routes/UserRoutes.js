9efe9ae3b5187f1bce495973f6f54a44ad720f6a
Felipe Campos <felipecampos50123@gmail.com>
2023-01-25 12:55:44 -0300

    Iniciando validações

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -4,6 +4,9 @@ const router = express.Router()
 //Controller
 const {register} = require("../controllers/UserController")
 
+//Middlewares
+const validate = require("../middlewares/handleValidation")
+
 //Routes
 router.post("/register", register)
 
