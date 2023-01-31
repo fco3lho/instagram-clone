2d67a6dd18111aac21be84e0126943af11bd7779
Felipe Campos <felipecampos50123@gmail.com>
2023-01-31 13:34:16 -0300

    Resgatando fotos do usuário

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -65,8 +65,18 @@ const getAllPhotos = async (req, res) => {
     res.status(200).json(photos)
 }
 
+//Get user photos
+const getUserPhotos = async (req, res) => {
+    const {id} = req.params
+
+    const photos = await Photo.find({userId: id}).sort([["createdAt", -1]]).exec()
+
+    return res.status(200).json(photos)
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
-    getAllPhotos
+    getAllPhotos,
+    getUserPhotos
 }
