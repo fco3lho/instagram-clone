3e5e9e623ba740f8002f8da627ad702fb9cda356
Felipe Campos <felipecampos50123@gmail.com>
2023-01-31 13:23:33 -0300

    Excluindo fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -23,11 +23,42 @@ const insertPhoto = async (req, res) => {
     //If photo was created successfully, return data
     if(!newPhoto){
         res.status(422).json({errors: ["Houve um problema, tente novamente mais tarde."]})
+        return
     }
 
     res.status(201).json(newPhoto)
 }
 
+//Remove photo from DB
+const deletePhoto = async(req, res) => {
+    const {id} = req.params
+
+    const reqUser = req.user
+
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+
+        //Check if photo exists
+        if(!photo){
+            res.status(404).json({errors: ["Foto não encontrada."]})
+            return
+        }
+
+        //Check if photo belongs to user
+        if(!photo.userId.equals(reqUser._id)){
+            res.status(422).json({errors: ["Houve um problema, tente novamente mais tarde."]})
+        }
+
+        await Photo.findByIdAndDelete(photo._id)
+
+        res.status(200).json({id: photo._id, message: "Foto excluída com sucesso."})
+    } catch (error) {
+        res.status(404).json({errors: ["Foto não encontrada."]})
+        return
+    }
+}
+
 module.exports = {
     insertPhoto,
+    deletePhoto
 }
