29615d0f4d63c4f38c5c828ee3ae0ccc00a105cc
Felipe Campos <felipecampos50123@gmail.com>
2023-01-31 14:20:41 -0300

    Configurando novo cluster

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,11 @@ mongoose.set('strictQuery', true)
 //connection
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASSWORD
+const idCluster = process.env.ID_CLUSTER
 
 const conn = async () => {
     try {
-        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.c866vjk.mongodb.net/?retryWrites=true&w=majority`)
+        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.${idCluster}.mongodb.net/?retryWrites=true&w=majority`)
         console.log("Conectou ao banco de dados.")
         return dbConn
     } catch (error) {
