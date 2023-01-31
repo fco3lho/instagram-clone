248097da06e65a370672934e26c19b32f8a58b12
Felipe Campos <felipecampos50123@gmail.com>
2023-01-31 20:57:28 -0300

    Comentário em foto

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -147,6 +147,41 @@ const likePhoto = async (req, res) => {
     res.status(200).json({photoId: id, userId: reqUser._id, message: "A foto foi curtida."})
 }
 
+//Comment functionally
+const commentPhoto = async (req, res) => {
+    const {id} = req.params
+    const {comment} = req.body
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id)
+
+    const photo = await Photo.findById(id)
+
+    //Check if photo exists
+    if(!photo){
+        res.status(404).json({errors: ["Foto não encontrada."]})
+        return
+    }
+
+    //Put comment in the array comments
+    const userComment = {
+        comment,
+        userName: user.name,
+        userImage: user.profileImage,
+        userId: user._id
+    }
+
+    photo.comments.push(userComment)
+    
+    await photo.save()
+
+    res.status(200).json({
+        comment: userComment,
+        message: "O comentário foi adicionado com sucesso."
+    })
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
@@ -154,5 +189,6 @@ module.exports = {
     getUserPhotos,
     getPhotoById,
     updatePhoto,
-    likePhoto
+    likePhoto,
+    commentPhoto
 }
