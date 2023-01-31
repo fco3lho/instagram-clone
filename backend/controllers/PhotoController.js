aa9b3005db45ddf88262ccf2046bcab6ed207fc0
Felipe Campos <felipecampos50123@gmail.com>
2023-01-31 13:26:37 -0300

    Resgatando todas as fotos

--- backend/controllers/PhotoController.js
+++ backend/controllers/PhotoController.js
@@ -58,7 +58,15 @@ const deletePhoto = async(req, res) => {
     }
 }
 
+//Get all photos
+const getAllPhotos = async (req, res) => {
+    const photos = await Photo.find({}).sort([["createdAt", -1]]).exec()
+
+    res.status(200).json(photos)
+}
+
 module.exports = {
     insertPhoto,
-    deletePhoto
+    deletePhoto,
+    getAllPhotos
 }
