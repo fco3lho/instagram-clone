cc2887775f64990e7406fd7bba257cca15f28701
Felipe Campos <felipecampos50123@gmail.com>
2023-02-02 13:43:33 -0300

    CSS Navbar e Footer

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,37 @@
-import "./Navbar.css"
+import "./Navbar.css";
 
 //Components
-import {NavLink, Link} from 'react-router-dom'
-import {BsSearch, BsHouseDoorFill, BsFillPersonFill, BsFillCameraFill} from 'react-icons/bs'
+import { NavLink, Link } from "react-router-dom";
+import {
+  BsSearch,
+  BsHouseDoorFill,
+  BsFillPersonFill,
+  BsFillCameraFill,
+} from "react-icons/bs";
 
 const Navbar = () => {
   return (
     <nav id="nav">
-        <Link to="/">Instagram</Link>
-        <form>
-            <BsSearch/>
-            <input type="text" />
-        </form>
-        <ul id="nav-links">
-            <NavLink to="">
-                <BsHouseDoorFill/>
-            </NavLink>
-            <NavLink to="/login">
-                Entrar
-            </NavLink>
-            <NavLink to="/register">
-                Cadastrar
-            </NavLink>
-        </ul>
+      <Link to="/">Instagram</Link>
+      <form id="search-form">
+        <BsSearch />
+        <input type="text" placeholder="Pesquisar" />
+      </form>
+      <ul id="nav-links">
+        <li>
+          <NavLink to="">
+            <BsHouseDoorFill />
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Entrar</NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
