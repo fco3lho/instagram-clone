814fced73dd482b4803b6dbebed80fa7f8b426ad
Felipe Campos <felipecampos50123@gmail.com>
2023-02-02 13:26:29 -0300

    Criando componente de navbar

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+        <p>Instagram &copy; {new Date().getFullYear()}</p>
+    </footer>
+  )
+}
+
+export default Footer
