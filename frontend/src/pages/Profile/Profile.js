d98130a5bb734432726d32f5ac019e59cfa90633
Felipe Campos <felipecampos50123@gmail.com>
2023-02-08 15:18:21 -0300

    Formulário de postar foto

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -16,36 +16,61 @@ import { useParams } from "react-router-dom";
 import { getUserDetails } from "../../slices/userSlice";
 
 const Profile = () => {
-  const {id} = useParams()
+  const { id } = useParams();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const {user, loading} = useSelector((state) => state.user)
-  const {user: userAuth} = useSelector((state) => state.auth)
+  const { user, loading } = useSelector((state) => state.user);
+  const { user: userAuth } = useSelector((state) => state.auth);
 
-  //Photo
-
+  //New form and edit form refs
+  const newPhotoForm = useRef();
+  const editPhotoForm = useRef();
 
   //Load user data
   useEffect(() => {
-    dispatch(getUserDetails(id))
-  }, [dispatch, id])
+    dispatch(getUserDetails(id));
+  }, [dispatch, id]);
 
-  if(loading){
-    return <p>Carregando...</p>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
   }
-  
-  return <div id="profile">
-    <div className="profile-header">
-      {user.profileImage && (
-        <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
-      )}
-      <div className="profile-description">
-        <h2>{user.name}</h2>
-        <p>{user.bio}</p>
+
+  return (
+    <div id="profile">
+      <div className="profile-header">
+        {user.profileImage && (
+          <img src={`${uploads}/users/${user.profileImage}`} alt={user.name} />
+        )}
+        <div className="profile-description">
+          <h2>{user.name}</h2>
+          <p>{user.bio}</p>
+        </div>
       </div>
+      {id === userAuth._id && (
+        <>
+          <div className="new-photo" ref={newPhotoForm}>
+            <h3>Compartilhe algum momento seu:</h3>
+            <form onSubmit={handleSubmit}>
+              <label>
+                <span>Título para a foto:</span>
+                <input type="text" placeholder="Insira um título" />
+              </label>
+              <label>
+                <span>Imagem:</span>
+                <input type="file" />
+              </label>
+              <input type="submit" value="Postar" />
+            </form>
+          </div>
+        </>
+      )}
     </div>
-  </div>;
+  );
 };
 
 export default Profile;
