f544186e166936335c16cd006e39441689715e53
Felipe Campos <felipecampos50123@gmail.com>
2023-02-07 22:51:39 -0300

    Exibindo imagem de preview

--- frontend/src/pages/EditProfile/EditProfile.js
+++ frontend/src/pages/EditProfile/EditProfile.js
@@ -43,19 +43,31 @@ const EditProfile = () => {
     e.preventDefault();
   };
 
+  const handleFile = (e) => {
+    //Image preview
+    const image = e.target.files[0]
+
+    setPreviewImage(image)
+
+    //Update image state
+    setProfileImage(image)
+  }
+
   return (
     <div id="edit-profile">
       <h2>Edite seus dados</h2>
       <p className="subtitle">
         Adicione uma imagem de perfil e conte mais sobre você.
       </p>
-      {/*Preview da imagem*/}
+      {(user.profileImage || previewImage) && (
+        <img className="profile-image" src={previewImage ? URL.createObjectURL(previewImage) : `${uploads}/users/${user.profileImage}`} alt={user.name}/>
+      )}
       <form onSubmit={handleSubmit}>
         <input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)} value={name || ""}/>
         <input type="email" placeholder="E-mail" disabled value={email || ""}/>
         <label>
           <span>Imagem do perfil:</span>
-          <input type="file" />
+          <input type="file" onChange={handleFile}/>
         </label>
         <label>
           <span>Bio:</span>
