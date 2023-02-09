c8c5dd9b508959e109f6b53bcc567270fbdfb2af
Felipe Campos <felipecampos50123@gmail.com>
2023-02-08 21:46:12 -0300

    Slice de inserção de foto

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -10,7 +10,22 @@ const initialState = {
   message: null,
 };
 
-//Functions
+//Publish user photo
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.publishPhoto(photo, token);
+
+    //Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -20,6 +35,26 @@ export const photoSlice = createSlice({
       state.message = null;
     },
   },
+  extraReducers: (builder) => {
+    builder
+      .addCase(publishPhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photo = action.payload;
+        state.photos.unshift(state.photo)
+        state.messa = "Foto publicada com sucesso."
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      })
+  }
 });
 
 export const { resetMessage } = photoSlice.actions;
