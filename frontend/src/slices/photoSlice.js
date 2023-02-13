3405980c5a49a302cada05049dc205626abfc738
Felipe Campos <felipecampos50123@gmail.com>
2023-02-13 14:09:17 -0300

    Slice de remoção de foto

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -26,6 +26,7 @@ export const publishPhoto = createAsyncThunk(
     return data;
   }
 );
+
 //Get user photos
 export const getUserPhotos = createAsyncThunk(
   "photo/userphotos",
@@ -33,10 +34,28 @@ export const getUserPhotos = createAsyncThunk(
     const token = thunkAPI.getState().auth.user.token;
 
     const data = await photoService.getUserPhotos(id, token);
+    
 
     return data;
   }
 );
+
+//Delete a photo
+export const deletePhoto = createAsyncThunk(
+  "photo/delete",
+  async(id, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.deletePhoto(id, token);
+
+    //Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+)
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -74,7 +93,25 @@ export const photoSlice = createSlice({
         state.success = true;
         state.error = null;
         state.photos = action.payload;
-      });
+      })
+      .addCase(deletePhoto.pending, (state) => {
+        state.loading = true;
+        state.error = false;
+      })
+      .addCase(deletePhoto.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.photos = state.photos.filter((photo) => {
+          return photo._id !== action.payload.id
+        })
+        state.message = action.payload.message
+      })
+      .addCase(deletePhoto.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+        state.photo = {};
+      })
   },
 });
 
