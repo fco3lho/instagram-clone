f18f28d0ba055a3b2e56dbf982adc8c20662da90
Felipe Campos <felipecampos50123@gmail.com>
2023-02-14 17:14:16 -0300

    Slice de comentário

--- frontend/src/slices/photoSlice.js
+++ frontend/src/slices/photoSlice.js
@@ -103,6 +103,27 @@ export const like = createAsyncThunk("photo/like", async (id, thunkAPI) => {
   return data;
 });
 
+//Add comment to a photo
+export const comment = createAsyncThunk(
+  "photo/comment",
+  async (photoData, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+
+    const data = await photoService.comment(
+      { comment: photoData.comment },
+      photoData.id,
+      token
+    );
+
+    //Check for errors
+    if (data.errors) {
+      return thunkAPI.rejectWithValue(data.errors[0]);
+    }
+
+    return data;
+  }
+);
+
 export const photoSlice = createSlice({
   name: "photo",
   initialState,
@@ -209,7 +230,19 @@ export const photoSlice = createSlice({
       .addCase(like.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
-        state.photo = {};
+      })
+      .addCase(comment.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+
+        state.photo.comments.push(action.payload.comment);
+
+        state.message = action.payload.message;
+      })
+      .addCase(comment.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
       });
   },
 });
