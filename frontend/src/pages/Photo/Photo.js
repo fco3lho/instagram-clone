0ba26fbfb70f4581f58f04e6e9cc0b97e3c5701b
Felipe Campos <felipecampos50123@gmail.com>
2023-02-14 14:14:32 -0300

    Implementando mensagem nas fotos

--- frontend/src/pages/Photo/Photo.js
+++ frontend/src/pages/Photo/Photo.js
@@ -11,6 +11,7 @@ import PhotoItem from "../../components/PhotoItem";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
+import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 
 //Redux
 import { getPhoto, like } from "../../slices/photoSlice";
@@ -20,6 +21,8 @@ const Photo = () => {
   const { id } = useParams();
 
   const dispatch = useDispatch();
+
+  const resetMessage = useResetComponentMessage(dispatch);
 
   const { user } = useSelector((state) => state.auth);
   const { photo, loading, error, message } = useSelector(
@@ -32,7 +35,9 @@ const Photo = () => {
   }, [dispatch, id]);
 
   const handleLike = () => {
-    dispatch(like(photo._id))
+    dispatch(like(photo._id));
+
+    resetMessage();
   };
 
   if (loading) {
@@ -43,6 +48,10 @@ const Photo = () => {
     <div id="photo">
       <PhotoItem photo={photo} />
       <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+      <div className="message-container">
+        {error && <Message msg={error} type="error" />}
+        {message && <Message msg={message} type="success" />}
+      </div>
     </div>
   );
 };
