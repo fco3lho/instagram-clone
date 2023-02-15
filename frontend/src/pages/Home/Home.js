35e2a60d39f5ec9bef50dcfb2e4965835d37dd01
Felipe Campos <felipecampos50123@gmail.com>
2023-02-15 11:28:59 -0300

    Finalizando home

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -14,30 +14,44 @@ import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
 import { getPhotos, like } from "../../slices/photoSlice";
 
 const Home = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const resetMessage = useResetComponentMessage()
+  const resetMessage = useResetComponentMessage(dispatch);
 
-  const {user} = useSelector((state) => state.auth)
-  const {photos, loading} = useSelector((state) => state.photo)
+  const { user } = useSelector((state) => state.auth);
+  const { photos, loading } = useSelector((state) => state.photo);
 
   //Load all photos
   useEffect(() => {
-    dispatch(getPhotos())
-  }, [dispatch])
+    dispatch(getPhotos());
+  }, [dispatch]);
 
   //Like a photo
   const handleLike = (photo) => {
-    dispatch(like(photo._id))
+    dispatch(like(photo._id));
 
-    resetMessage()
+    resetMessage();
+  };
+
+  if (loading) {
+    return <p>Carregando...</p>;
   }
 
-  if(loading){
-    return <p>Carregando...</p>
-  }
-
-  return <div>Home</div>;
+  return (
+    <div id="home">
+      {photos && photos.map((photo) => (<div key={photo._id}>
+        <PhotoItem photo={photo}/>
+        <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
+        <Link className="btn" to={`/photos/${photo._id}`}>Ver mais</Link>
+      </div>))}
+      {photos && photos.length === 0 && (
+        <h2 className="no-photos">
+          Ainda não há fotos publicadas,{" "}
+          <Link to={`/users/${user.userId}`}>clique aqui</Link>.
+        </h2>
+      )}
+    </div>
+  );
 };
 
 export default Home;
