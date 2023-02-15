614e60c06c4b82ea89d5153525ce7be37b88bcae
Felipe Campos <felipecampos50123@gmail.com>
2023-02-15 13:34:49 -0300

    Implementando formulário de busca

--- frontend/src/pages/Search/Search.js
+++ frontend/src/pages/Search/Search.js
@@ -4,6 +4,7 @@ import "./Search.css";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useResetComponentMessage } from "../../hooks/useResetComponentMessage";
+import { useQuery } from "../../hooks/useQuery";
 
 //Components
 import LikeContainer from "../../components/LikeContainer";
@@ -11,7 +12,6 @@ import PhotoItem from "../../components/PhotoItem";
 import { Link } from "react-router-dom";
 
 //Redux
-
 
 const Search = () => {
   return <div>Search</div>;
