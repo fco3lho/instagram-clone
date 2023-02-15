7d9e12e7b75ccfe267e40c6ee283a5db94304a54
Felipe Campos <felipecampos50123@gmail.com>
2023-02-15 13:57:17 -0300

    Concluindo busca de fotos

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -39,11 +39,16 @@ const Home = () => {
 
   return (
     <div id="home">
-      {photos && photos.map((photo) => (<div key={photo._id}>
-        <PhotoItem photo={photo}/>
-        <LikeContainer photo={photo} user={user} handleLike={handleLike}/>
-        <Link className="btn" to={`/photos/${photo._id}`}>Ver mais</Link>
-      </div>))}
+      {photos &&
+        photos.map((photo) => (
+          <div key={photo._id}>
+            <PhotoItem photo={photo} />
+            <LikeContainer photo={photo} user={user} handleLike={handleLike} />
+            <Link className="btn" to={`/photos/${photo._id}`}>
+              Ver mais
+            </Link>
+          </div>
+        ))}
       {photos && photos.length === 0 && (
         <h2 className="no-photos">
           Ainda não há fotos publicadas,{" "}
